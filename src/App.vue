ac19d93a3ee5f3cb66d28b1ba91f4b383b4ef283
Oscar Calle <gameplaysoscar@gmail.com>
2021-12-21 15:36:01 -0500

    First commit - Initial config: eslint + tailwindCSS

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1 class="text-3xl font-bold underline">Hola mundo</h1>
+    <h3>{{ counter }}</h3>
+</template>
+
+<script setup>
+import { ref } from "vue";
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+
+const counter = ref(5);
+</script>
